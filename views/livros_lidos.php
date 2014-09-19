2040df29e208f7d4fd4da5d8c9d8e3b468c45c9e
admin@letrastrocadas.com.br <admin@letrastrocadas.com.br@b4fbbe38-814c-462a-9e6d-be7759f762d5>
2014-09-19 14:01:11 +0000

    Livros lidos

--- views/livros_lidos.php
+++ views/livros_lidos.php
@@ -5,36 +5,26 @@
 		include("classes/class_banco.php");
 		include("classes/class_pesquisar.php");
 		
-		$codigo_ultimo = $_GET['livro'];
-		if(!empty($codigo_ultimo))
-		{
-			$codigo = $codigo_ultimo;
-		}
-		else
-		{
-			$codigo = "0";
-		}
-		
 		$bd = new Banco();
 		$campos = "id_marcacao,imagem_livros,livro.nome AS Livro,autor.nome AS Autor,editora.nome As Editora, livro.sinopse As sinopse";
 		$tabelas = "tbl_marcacao marcacao INNER JOIN tbl_livro livro INNER JOIN tbl_editora editora INNER JOIN tbl_autor autor ON id_marcacao = 2 AND id_editora = editora_id AND id_autor = autor_id AND id_livro = livro_id";
-		$pesquisar_livros = new Pesquisar($tabelas,$campos,"id_marcacao > ".$codigo." AND usuario_id =".$_SESSION['id']." LIMIT 7");
+		$pesquisar_livros = new Pesquisar($tabelas,$campos,"tipo = 2 AND usuario_id =".$_SESSION['id']);
 		$resultado = $pesquisar_livros->pesquisar();
-		
-		$pesquisar_quantidade = new Pesquisar($tabelas,"COUNT(id_marcacao) As quantidade","usuario_id =".$_SESSION['id']);
+
+		$pesquisar_quantidade = new Pesquisar($tabelas,"COUNT(id_marcacao) As quantidade","tipo = 2 AND usuario_id =".$_SESSION['id']);
 		$resultado_quantidade = $pesquisar_quantidade->pesquisar();
 		
 		$pesquisa_quantidade=mysql_fetch_array($resultado_quantidade);
 		$quantidade = $pesquisa_quantidade['quantidade'];
 		
-		$id =array();
+		$id = array();
 		$nome = array();
 		$imagem = array();
 		$editora = array();
 		$autor = array();
 		$sinopse = array();
 		
-		while($pesquisa=mysql_fetch_array($resultado))
+		while($pesquisa=mysql_fetch_assoc($resultado))
 		{
 			$id[] = $pesquisa['id_marcacao'];
 			$nome[] = $pesquisa['Livro'];
@@ -66,55 +56,50 @@
 
 
 <article style = "width:80%;  margin-left:10%;">
-     <section class="panel panel-default" >
-        <section class="panel-heading">Livros lidos</section>
-		
-             <section class="panel-body">
-			    <section class = "row">
-					<section class = "col-lg-4" style = "width: auto;">	
+	<section class="panel panel-default" >
+		<section class="panel-heading">Livros lidos</section>
+		<section class="panel-body">
+			<?php
+				if($quantidade >= 1)
+				{
+			?>
+			<section class = "row">
+				<section class = "col-lg-4" style = "width: auto;">	
 					<section class = "bs-component"> 
-							<a class = "thumbnail">
-								<img src = "<?php echo $imagem[0];?>" alt = "<?php echo $nome[0];?>" height = "177px" width = "120px"/> 
-							</a>
+						<a class = "thumbnail">
+							<img src = "<?php echo $imagem[0];?>" alt = "<?php echo $nome[0];?>" height = "177px" width = "120px"/> 
+						</a>
 					</section>
-					</section>
-					<section class = "col-lg-4">
-							<a> <h3> <?php echo utf8_encode($nome[0]); ?> </h3> </a>				  
-							<a> <h4> <?php echo utf8_encode($autor[0]); ?> </h4></a>
-						    <a> <h5> <?php echo utf8_encode($editora[0]); ?> </h5></a>
-							<form method="post" action="?url=alterar_livro_usuario&cod=<?php echo $id[0];?>">
-							<input type="submit" class="btn btn-primary btn-sm" name="alterarlivro" value="Alterar Livro"/>
-							</form>
-					</section>
-					<section class = "col-lg-4" style = "width:48%;">
-						<textarea class="form-control" rows="9" readonly>
-						<?php echo utf8_encode($sinopse[0]);?>
-						</textarea>
-					</section> 
-					
 				</section>
-				<br>
-				   <?php
-					
-						for($contador=0;$contador<=$quantidade-1;$contador++)
-						{
-							echo '<img src = "'.$imagem[$contador].'" alt = "'.$nome[$contador].'" height = "177px" width = "120px">'; 
-						}
-					
-					?>
-
-                   <ul class="pager">
-                        <li class="previous disabled"><a href="#">← Anterior</a></li>
-                        <li class="next"><a href="?url=livros_lidos&livro= <?php 
-																					if(!$quantidade < 7)
-																					echo $id[6];
-																				?>">Próximo →</a></li>
-                   </ul>
-				   
-             </section>
-			 
-       </section>
-
-	   
-	  
+				<section class = "col-lg-4">
+					<a> <h3> <?php echo utf8_encode($nome[0]); ?> </h3> </a>				  
+					<a> <h4> <?php echo utf8_encode($autor[0]); ?> </h4></a>
+					<a> <h5> <?php echo utf8_encode($editora[0]); ?> </h5></a>
+					<form method="post" action="?url=alterar_livro_usuario&cod=<?php echo $id[0];?>">
+						<input type="submit" class="btn btn-primary btn-sm" name="alterarlivro" value="Alterar Livro"/>
+					</form>
+				</section>
+				<section class = "col-lg-4" style = "width:48%;">
+					<textarea class="form-control" rows="9" readonly>
+						<?php echo utf8_encode($sinopse[0]);?>
+					</textarea>
+				</section> 
+			</section>
+			<?php
+					for($contador=0;$contador<=$quantidade-1;$contador++)
+					{
+						echo '<img src = "'.$imagem[$contador].'" alt = "'.$nome[$contador].'" height = "177px" width = "120px">'; 
+					}
+				}
+				else
+				{
+			?>
+			<section class="alert alert-dismissable alert-info">
+				<strong>Você não adicionou nenhum livro a sua estante de livros lidos! Para adicionar é só pesquisar o livro, ir no botão "Eu..." e clicar em "Lido"!</strong>
+			</section>
+			<?php
+				}
+			?>
+		</section>
+	</section>
 </article>
