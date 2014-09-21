b25668e579fc1b2e8ea2593b2a779a5a3be7e348
Camilo Santana <camilocsm@gmail.com>
2014-09-21 10:51:04 +0000

    Livros lidos

--- views/livros_lidos.php
+++ views/livros_lidos.php
@@ -1,3 +1,22 @@
+<script type="text/javascript">
+	function Abrir(id)
+	{
+		alert('oi');
+		$.ajax({
+			
+			url : "ajax/retorna_lista.php?livro="+id,
+			dataType : "json",
+			success : function(data){
+				document.getElementById('livro').innerHTML = data.section;
+			},
+			error : function(data){
+			alert("Ops! Ocorreu um erro, contate nossos administradores para mais informações.");
+			}
+		
+		});
+		
+	}
+</script>
 <?php
 	//Verifica se o usuário tem acesso à essa página
 	if($_SESSION['nivel_acesso'] == 1)
@@ -6,8 +25,8 @@
 		include("classes/class_pesquisar.php");
 		
 		$bd = new Banco();
-		$campos = "id_marcacao,imagem_livros,livro.nome AS Livro,autor.nome AS Autor,editora.nome As Editora, livro.sinopse As sinopse";
-		$tabelas = "tbl_marcacao marcacao INNER JOIN tbl_livro livro INNER JOIN tbl_editora editora INNER JOIN tbl_autor autor ON id_marcacao = 2 AND id_editora = editora_id AND id_autor = autor_id AND id_livro = livro_id";
+		$campos = "id_marcacao,imagem_livros,id_livro,livro.nome AS Livro,autor.nome AS Autor,editora.nome As Editora, livro.sinopse As sinopse";
+		$tabelas = "tbl_marcacao marcacao INNER JOIN tbl_livro livro INNER JOIN tbl_editora editora INNER JOIN tbl_autor autor ON id_editora = editora_id AND id_autor = autor_id AND id_livro = livro_id";
 		$pesquisar_livros = new Pesquisar($tabelas,$campos,"tipo = 2 AND usuario_id =".$_SESSION['id']);
 		$resultado = $pesquisar_livros->pesquisar();
 
@@ -17,6 +36,7 @@
 		$pesquisa_quantidade=mysql_fetch_array($resultado_quantidade);
 		$quantidade = $pesquisa_quantidade['quantidade'];
 		
+		$id_livro = array();
 		$id = array();
 		$nome = array();
 		$imagem = array();
@@ -26,6 +46,7 @@
 		
 		while($pesquisa=mysql_fetch_assoc($resultado))
 		{
+			$id_livro[] = $pesquisa['id_livro'];
 			$id[] = $pesquisa['id_marcacao'];
 			$nome[] = $pesquisa['Livro'];
 			$imagem[] = $pesquisa['imagem_livros'];
@@ -33,6 +54,8 @@
 			$autor[] = $pesquisa['Autor'];
 			$sinopse[] = $pesquisa['sinopse'];
 		}
+
+		$aspas="'";
 	}
 	else
 	{	
@@ -63,7 +86,7 @@
 				if($quantidade >= 1)
 				{
 			?>
-			<section class = "row">
+			<section class = "row" id="livro">
 				<section class = "col-lg-4" style = "width: auto;">	
 					<section class = "bs-component"> 
 						<a class = "thumbnail">
@@ -75,20 +98,25 @@
 					<a> <h3> <?php echo utf8_encode($nome[0]); ?> </h3> </a>				  
 					<a> <h4> <?php echo utf8_encode($autor[0]); ?> </h4></a>
 					<a> <h5> <?php echo utf8_encode($editora[0]); ?> </h5></a>
-					<form method="post" action="?url=alterar_livro_usuario&cod=<?php echo $id[0];?>">
+					<form method="post" action="?url=alterar_livro_usuario&cod=<?php echo $id_livro[0];?>">
 						<input type="submit" class="btn btn-primary btn-sm" name="alterarlivro" value="Alterar Livro"/>
 					</form>
 				</section>
 				<section class = "col-lg-4" style = "width:48%;">
 					<textarea class="form-control" rows="9" readonly>
-						<?php echo utf8_encode($sinopse[0]);?>
+						<?php echo $sinopse[0];?>
 					</textarea>
 				</section> 
 			</section>
 			<?php
 					for($contador=0;$contador<=$quantidade-1;$contador++)
 					{
-						echo '<img src = "'.$imagem[$contador].'" alt = "'.$nome[$contador].'" height = "177px" width = "120px">'; 
+						echo '
+							<section class="col-lg-2">
+								<a class = "thumbnail" onClick="Abrir('.$aspas.''.$id_livro[$contador].''.$aspas.')">
+									<img src = "'.$imagem[$contador].'" alt = "'.$nome[$contador].'" height = "177px" width = "120px"/> 
+								</a>
+							</section>'; 
 					}
 				}
 				else
