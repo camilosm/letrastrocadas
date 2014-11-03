c92994ecbcecc5d43e19e0e2721f2fdc71ffc4ef
admin@letrastrocadas.com.br <admin@letrastrocadas.com.br@b4fbbe38-814c-462a-9e6d-be7759f762d5>
2014-11-03 11:11:04 +0000

    favicon

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<head>
 
 		<title>Letras Trocadas</title>
-		<link rel="shortcut icon" href="content/ico/icone.ico">
+		<link rel="shortcut icon" href="content/ico/favicon.ico">
 		<meta charset="UTF-8"/>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
